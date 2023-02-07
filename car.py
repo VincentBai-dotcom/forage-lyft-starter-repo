fd7df74d7b9ed98ee1187981a3f2b49e4ff410ca
Vincent Bai <butcheryyy@gmail.com>
2023-02-07 01:09:21 -0500

    task 2

--- car.py
+++ car.py
@@ -1,10 +1,17 @@
-from abc import ABC, abstractmethod
+from battery import Battery
+from engine import Engine
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(Engine,Battery):
+    def __init__(self, battery,engine):
+        self.battery = battery
+        self.engine = engine
 
-    @abstractmethod
+    def battery_needs_service(self):
+        return self.battery.needs_service()  
+    
+    def engine_needs_service(self):
+        return self.engine.needs_service()
+
     def needs_service(self):
-        pass
+        return self.battery_needs_service() or self.engine_needs_service()
